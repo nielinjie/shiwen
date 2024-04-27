78a59b6ffd6de2b9c42dc480363698cb93d6d2a8
nielinjie <nielinjie@gmail.com>
2024-04-27 11:21:43 +0800

    国内云环境依赖调整

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ java {
 }
 
 repositories {
+    maven("https://maven.aliyun.com/repository/public/")
+    maven("https://maven.aliyun.com/repository/spring/")
+    mavenLocal()
     mavenCentral()
     maven("https://repo.spring.io/milestone")
     maven("https://repo.spring.io/snapshot")
@@ -78,7 +81,7 @@ task("copyFiles", type = Copy::class) {
 
     // dependsOn("yarnBuild") 在构建环境没有yarn，所以在本地构建，结果上传到git。
 }
-task("copyDocs", type = Copy::class){
+task("copyDocs", type = Copy::class) {
     from("./README.MD")
     into("src/main/resources/markdowns")
 }
@@ -89,8 +92,8 @@ task("copyDocs", type = Copy::class){
 //     commandLine(yarn, "build")
 // }
 
-tasks.processResources{
-    dependsOn("copyFiles","copyDocs")
+tasks.processResources {
+    dependsOn("copyFiles", "copyDocs")
 }
 
 
